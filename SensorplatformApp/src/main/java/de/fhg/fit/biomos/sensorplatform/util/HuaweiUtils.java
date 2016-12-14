259a1ac4f5e0f50ab45e7680e4aa101f059d9423
Daniel Pyka <daniel.pyka@fit.fraunhofer.de>
2016-12-14 09:40:17 +0100

    Add remaining huawei system modes

--- SensorplatformApp/src/main/java/de/fhg/fit/biomos/sensorplatform/util/HuaweiUtils.java
+++ SensorplatformApp/src/main/java/de/fhg/fit/biomos/sensorplatform/util/HuaweiUtils.java
@@ -13,8 +13,11 @@ public class HuaweiUtils {
 
   public static final String SYSTEM_MODE_0 = "No services";
   public static final String SYSTEM_MODE_3 = "GSM/GPRS mode";
+  public static final String SYSTEM_MODE_4 = "HDR mode";
   public static final String SYSTEM_MODE_5 = "WCDMA mode";
+  public static final String SYSTEM_MODE_6 = "GPS mode";
   public static final String SYSTEM_MODE_7 = "GSM/WCDMA mode";
+  public static final String SYSTEM_MODE_15 = "TD-SCDMA mode";
 
   public static final String SYSTEM_SUBMODE_0 = "No services";
   public static final String SYSTEM_SUBMODE_1 = "GSM mode";
@@ -24,6 +27,10 @@ public class HuaweiUtils {
   public static final String SYSTEM_SUBMODE_5 = "HSDPA mode";
   public static final String SYSTEM_SUBMODE_6 = "HSUPA mode";
   public static final String SYSTEM_SUBMODE_7 = "HSUPA and HSDPA mode";
+  public static final String SYSTEM_SUBMODE_8 = "TD_SCDMA mode";
+  public static final String SYSTEM_SUBMODE_9 = "HSPA+ mode";
+  public static final String SYSTEM_SUBMODE_17 = "HSPA+(64QAM) mode";
+  public static final String SYSTEM_SUBMODE_18 = "HSPA+(MIMO) mode";
 
   /**
    * Get the name of the mode the surf stick is in, represented by an ID.
@@ -38,10 +45,14 @@ public class HuaweiUtils {
         return SYSTEM_MODE_0;
       case 3:
         return SYSTEM_MODE_3;
+      case 4:
+        return SYSTEM_MODE_4;
       case 5:
         return SYSTEM_MODE_5;
       case 7:
         return SYSTEM_MODE_7;
+      case 15:
+        return SYSTEM_MODE_15;
       default:
         return "invalid";
     }
@@ -72,6 +83,14 @@ public class HuaweiUtils {
         return SYSTEM_SUBMODE_6;
       case 7:
         return SYSTEM_SUBMODE_7;
+      case 8:
+        return SYSTEM_SUBMODE_8;
+      case 9:
+        return SYSTEM_SUBMODE_9;
+      case 10:
+        return SYSTEM_SUBMODE_17;
+      case 11:
+        return SYSTEM_SUBMODE_18;
       default:
         return "invalid";
     }
